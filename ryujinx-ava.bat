475ce6ea02e2ba419c5633ce3abece47a3aab4b8
Renn <8340896+AkazaRenn@users.noreply.github.com>
2024-04-24 21:52:05 -0400

    Update ryujinx-ava.bat

--- ryujinx-ava.bat
+++ ryujinx-ava.bat
@@ -1,9 +1,9 @@
 @echo off
 set "SCRIPT_DIR=%~dp0"
 set "GH_API_URL=https://api.github.com/repos/Ryujinx/release-channel-master/releases/latest"
-set "RYU_REGEX=^test-ava-ryujinx-.*-win_x64.zip$"
+set "RYU_REGEX=^^test-ava-ryujinx-.*-win_x64.zip$"
 set "RYU_EXE_PATH=%SCRIPT_DIR%publish\Ryujinx.exe"
 
-powershell -ExecutionPolicy Bypass -Command "$latest_release = Invoke-RestMethod -Uri '%GH_API_URL%'; $asset = $latest_release.assets | Where-Object { $_.name -match '%RYU_REGEX%' }; if ($asset) { $asset_url = $asset.browser_download_url; $file_name = $asset.name; $file_path = Join-Path '%SCRIPT_DIR%' $file_name; if (!((Test-Path -Path '%RYU_EXE_PATH%') -and ([System.Diagnostics.FileVersionInfo]::GetVersionInfo('%RYU_EXE_PATH%').FileVersion -ge $latest_release.tag_name))) { Invoke-WebRequest -Uri $asset_url -OutFile $file_path; Expand-Archive -Path $file_path -DestinationPath '%SCRIPT_DIR%' -Force } }"
+powershell -ExecutionPolicy Bypass -Command "$latest_release = Invoke-RestMethod -Uri '%GH_API_URL%'; $asset = $latest_release.assets | Where-Object { $_.name -match '%RYU_REGEX%' }; if ($asset) { $asset_url = $asset.browser_download_url; $file_name = $asset.name; $file_path = Join-Path '%SCRIPT_DIR%' $file_name; if (!((Test-Path -Path '%RYU_EXE_PATH%') -and ([System.Version][System.Diagnostics.FileVersionInfo]::GetVersionInfo('%RYU_EXE_PATH%').FileVersion -ge [System.Version]$latest_release.tag_name))) { Invoke-WebRequest -Uri $asset_url -OutFile $file_path; Expand-Archive -Path $file_path -DestinationPath '%SCRIPT_DIR%' -Force } }"
 
 start "" /B "%RYU_EXE_PATH%" %*
