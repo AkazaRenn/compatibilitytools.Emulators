a363498365f398bc1a4b77ad2fa1899ead08c630
Renn <8340896+AkazaRenn@users.noreply.github.com>
2025-01-23 14:45:24 +0000

    Fix updating from another cwd

--- ryujinx.sh
+++ ryujinx.sh
@@ -2,10 +2,10 @@
 
 SCRIPT_DIR=$(dirname $(realpath $0))
 RELEASE_ID_PATH=${SCRIPT_DIR}/release_id
-GAME_FILE_PATH=${SCRIPT_DIR}/publish
-EXECUTABLE_PATH=${GAME_FILE_PATH}/Ryujinx.sh
+DOWNLOADED_FILE_PATH=${SCRIPT_DIR}/publish
+EXECUTABLE_PATH=${DOWNLOADED_FILE_PATH}/Ryujinx.sh
 ARCHIVE_FILENAME_PATTERN="^ryujinx-.*-linux_x64.tar.gz$"
-ARCHIVE_EXTRACT_CMD="tar -xf"
+ARCHIVE_EXTRACT_CMD="tar -C ${SCRIPT_DIR} -xf"
 GITHUB_RELEASE_URI="https://api.github.com/repos/Ryubing/Ryujinx/releases/latest"
 
 latest_release=`curl -s ${GITHUB_RELEASE_URI} | jq -r '.assets[] | select(.name | test('\"${ARCHIVE_FILENAME_PATTERN}\"'))'`
@@ -19,7 +19,7 @@ if [ "${latest_release}" != "" ]; then
     if [[ ${latest_release_id} != ${current_release_id} ]] || [[ ! -f ${EXECUTABLE_PATH} ]]; then
         archive_download_path=${SCRIPT_DIR}/download.temp
         echo ${latest_release} | jq -r '.browser_download_url' | wget -qO ${archive_download_path} -i -
-        rm -rf ${GAME_FILE_PATH}
+        rm -rf ${DOWNLOADED_FILE_PATH}
         ${ARCHIVE_EXTRACT_CMD} ${archive_download_path}
         echo ${latest_release_id} > ${RELEASE_ID_PATH}
         rm -rf ${archive_download_path}
--- shadPS4.sh
+++ shadPS4.sh
@@ -2,10 +2,10 @@
 
 SCRIPT_DIR=$(dirname $(realpath $0))
 RELEASE_ID_PATH=${SCRIPT_DIR}/release_id
-GAME_FILE_PATH=${SCRIPT_DIR}/Shadps4-qt.AppImage
-EXECUTABLE_PATH=${GAME_FILE_PATH}
+DOWNLOADED_FILE_PATH=${SCRIPT_DIR}/Shadps4-qt.AppImage
+EXECUTABLE_PATH=${DOWNLOADED_FILE_PATH}
 ARCHIVE_FILENAME_PATTERN="^shadps4-linux-qt-.*.zip$"
-ARCHIVE_EXTRACT_CMD="unzip"
+ARCHIVE_EXTRACT_CMD="unzip -d ${SCRIPT_DIR}"
 GITHUB_RELEASE_URI="https://api.github.com/repos/shadps4-emu/shadPS4/releases/latest"
 
 latest_release=`curl -s ${GITHUB_RELEASE_URI} | jq -r '.assets[] | select(.name | test('\"${ARCHIVE_FILENAME_PATTERN}\"'))'`
@@ -19,7 +19,7 @@ if [ "${latest_release}" != "" ]; then
     if [[ ${latest_release_id} != ${current_release_id} ]] || [[ ! -f ${EXECUTABLE_PATH} ]]; then
         archive_download_path=${SCRIPT_DIR}/download.temp
         echo ${latest_release} | jq -r '.browser_download_url' | wget -qO ${archive_download_path} -i -
-        rm -rf ${GAME_FILE_PATH}
+        rm -rf ${DOWNLOADED_FILE_PATH}
         ${ARCHIVE_EXTRACT_CMD} ${archive_download_path}
         echo ${latest_release_id} > ${RELEASE_ID_PATH}
         rm -rf ${archive_download_path}
